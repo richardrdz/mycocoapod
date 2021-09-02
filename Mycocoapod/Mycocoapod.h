92048eed3ff46d5624d74eccb08c272e7de8ec76
Richard Eduardo Rodriguez Ortiz <richard.rodriguez@banregio.com>
2021-09-02 18:37:59 -0500

    Initial Commit

---
+++ Mycocoapod/Mycocoapod.h
@@ -0,0 +1,18 @@
+//
+//  Mycocoapod.h
+//  Mycocoapod
+//
+//  Created by Richard Eduardo Rodriguez Ortiz on 02/09/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Mycocoapod.
+FOUNDATION_EXPORT double MycocoapodVersionNumber;
+
+//! Project version string for Mycocoapod.
+FOUNDATION_EXPORT const unsigned char MycocoapodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Mycocoapod/PublicHeader.h>
+
+

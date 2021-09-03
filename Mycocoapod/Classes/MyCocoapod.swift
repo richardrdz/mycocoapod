0aac457be195fb4216d515e284c67e8a1400200b
Richard Eduardo Rodriguez Ortiz <richard.rodriguez@banregio.com>
2021-09-02 19:12:50 -0500

    x
    x

---
+++ Mycocoapod/Classes/MyCocoapod.swift
@@ -0,0 +1,13 @@
+//
+//  MyCocoapod.swift
+//  Mycocoapod
+//
+//  Created by Richard Eduardo Rodriguez Ortiz on 02/09/21.
+//
+
+import Foundation
+public class MyCocoapod {
+    public func helloWorld() {
+        print("MY COCOAPOD!!!!")
+    }
+}
